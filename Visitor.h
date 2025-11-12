979752e72ed1d15899758b0f687826caceee147a
youssefabdelatif <youssefabdelatif@icloud.com>
2025-11-12 14:54:53 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -5,6 +5,18 @@
 using namespace std;
 
 //define visitor class here
+class reptile:public animal{
+  protected:
+  bool isVen;
+  
+  public:
+  reptile(string n,int a,bool isH,bool isV){
+  name =n;
+  age=a;
+  isHungry=isH;
+  isVen=isV;
+  }
+}
 
 
 
