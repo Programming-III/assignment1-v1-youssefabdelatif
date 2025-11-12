dfde63216cf05ffd33b829e486cfb67a9e43df25
youssefabdelatif <youssefabdelatif@icloud.com>
2025-11-12 14:53:31 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -2,6 +2,29 @@
 #define ENCLOSURE_H
 
 #include "Animal.h"
+class Enclosure{
+  protected:
+  Animal* animals;
+  int CurentCount;
+  int capacity;
+  
+  public:
+  Enclosure(Animal* animals,int CurentCount,int capacity){
+    Animal* getanimals();
+    int getCurrentCount();
+    int getcapacity();
+  }
+  void addAnimal(Animal*animals){
+    
+    
+  }
+  void displayAnimals(){
+    return animals;
+  }
+  
+  
+}
+
 
 //define enclosure class here 
 
