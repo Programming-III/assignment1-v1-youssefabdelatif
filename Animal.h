f59c77686545eec109a83de293d76fd6ff5eeb8b
youssefabdelatif <youssefabdelatif@icloud.com>
2025-11-12 14:51:49 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -5,7 +5,35 @@
 using namespace std;
 
 //define animal class here 
+class animal{
+ protected:
+  string name;
+  int age;
+  bool isHungry;
+  
+  public:
+  animal(string n,int a,bool isH){
 
+  name =n;
+  age=a;
+  isHungry=isH;
+  }
+  void display(){
+    cout << name>>;
+    cout <<age>>;
+    cout <<isHungry>>;
+    
+  }
+  void feed(){
+    if(isHungry==true){
+      cout << "feed animal" <<;
+    }
+    else{
+      cout <<"animal is fed"<<;
+    }
+  }
+  
+}
 
 
 #endif
