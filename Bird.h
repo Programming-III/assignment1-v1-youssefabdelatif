4281dba78781b26c8277b9d861385a81facbaf62
youssefabdelatif <youssefabdelatif@icloud.com>
2025-11-12 14:52:29 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -4,7 +4,19 @@
 #include "Animal.h"
 
 //define class bird here 
-
+class Bird:public animal{
+  protected:
+  float wingSpan;
+  
+  public:
+  bird(string name,int a,bool isH,float ws){
+  name =n;
+  age=a;
+  isHungry=isH;
+  wingSpan=ws;
+  }
+  
+}
 
 
 
