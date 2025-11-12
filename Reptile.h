4815b59a56c3773d265231d025ccdba0aff5ddc2
youssefabdelatif <youssefabdelatif@icloud.com>
2025-11-12 14:54:03 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -5,6 +5,18 @@
 
 //define reptile class here
 
+class reptile:public animal{
+  protected:
+  bool isVen;
+  
+  public:
+  reptile(string n,int a,bool isH,bool isV){
+  name =n;
+  age=a;
+  isHungry=isH;
+  isVen=isV;
+  }
+}
 
 
 
